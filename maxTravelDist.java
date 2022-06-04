1c1d7d1146a784fd0a1d172f2d0c343eb9cf0b49
r-ashwini <34915478+r-ashwini@users.noreply.github.com>
2022-06-03 21:30:56 -0700

    Create maxTravelDist.java

---
+++ maxTravelDist.java
@@ -0,0 +1,9 @@
+/*
+1. Brute force double for loop will check all combinations even if there are duplicates  TC O(m *n)
+2. Two pointer approach where low is start of one array and high is at end of another array , can skip duplicate distances
+3. hashing will not work , because the sum is not fixed 
+4. Binary search will have TC O(m logn) 
+
+  
+  
+  */
